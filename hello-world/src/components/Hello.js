bf5e39a515411c8c85640982f806d1233b1ace5f
kmohd7121 <kmohd7121@gmail.com>
2021-10-18 16:44:29 +0530

    console kalam

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -6,6 +6,7 @@ const Hello = () =>{
 //             <h1>Hello Rehan</h1>
 //         </div>
 //     )
+    console.log('mohd kalam hiiiiiiiiiiiiiii.........')
     return React.createElement('div' ,{id:'hello',className:'dummyClass'},createElement('h1',null,'Hello Farhan'))
 } 
 export default Hello
